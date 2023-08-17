bc9e88b51e879b408c3aae9beb6e2dc0a855b5f6
GABRIEL DO NASCIMENTO QUINTILIANO <gabriel.quintiliano@granbery.edu.dti>
2023-08-16 22:14:05 -0300

    atualizei

---
+++ dicionario.py
@@ -0,0 +1,4 @@
+nome1 = {"nome" :"gabriel", "idade": 26 , "telefone" : 32932323}
+nome2 = {"nome" :"wechiley", "idade": 31 , "telefone" : 32932323231}
+nome3 = {"nome" :"felipe", "idade": 29 , "telefone" : 32932323233}
+print(nome1["nome"])
